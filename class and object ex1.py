b44054a1f75fcabc865932a98ce28b5a9af76b57
sudhanasuviharan2609 <sudhanasuviharan2609@gmail.com>
2025-11-07 15:50:14 +0530

    created

---
+++ class and object ex1.py
@@ -0,0 +1,24 @@
+class laptop:
+    price=0
+    processor=""
+    ram=""
+
+hp=laptop()
+dell=laptop()
+leno=laptop()
+
+hp.price="50000"
+hp.processor="i5"
+hp.ram="8gb"
+
+dell.price=56000
+dell.processor="i7"
+dell.ram="16gb"
+
+leno.price=60000
+leno.processor="i9"
+leno.ram="8gb"
+
+print("hp",hp.processor)
+print(hp.price)
+print(hp.ram)
